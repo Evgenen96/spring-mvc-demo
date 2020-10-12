ef467ad5350fab88f7d04cd4ca475913272e1317
Eugene Nikonov <nikonovse96@gmail.com>
2020-10-12 17:22:03 +0400

    string validation

---
+++ src/ru/evgenen96/springdemo/mvc/CustomerController.java
@@ -0,0 +1,42 @@
+package ru.evgenen96.springdemo.mvc;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    // add an initBinder to convert trim input strings
+    // resolve issue for the validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @RequestMapping("/showForm")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theResult) {
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+        if (theResult.hasErrors()) {
+            return "customer-form";
+        }
+        return "customer-confirmation";
+    }
+}
